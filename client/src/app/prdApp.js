eef688faa2df232ca3c9535394e1389d1ba1edb2
Ramee <ramesh@giantleapsystems.com>
2013-09-11 10:18:03 +0530

    Add client code

---
+++ client/src/app/prdApp.js
@@ -0,0 +1,38 @@
+/**
+
+*/
+'use strict';
+
+var subscribeModule = angular.module('prd.subscribe', []);
+var surveyModule = angular.module('prd.survey', []);
+var adminModule = angular.module('prd.admin', []);
+var reportsModule = angular.module('prd.reports', []);
+
+angular.module('prd.mainapp',['subscribeModule', 'surveyModule', 
+    'adminModule', 'reportsModule', 'ngResource'])
+.config (['$routeProvider'], function($routeProvider) {
+    $routeProvider.
+        when('/',
+                {
+                    templateUrl: 'index.html',
+                    controller: 'prd.main.controller'
+                } 
+            ).
+        when ('/subscribe',
+                {
+                    templateUrl: 'subscribe/views/subscribe.html',
+                    controller: 'prd.subscribe.controller'
+                }             
+            ).
+        when('/survey'
+                {
+                    templateUrl: 'survey/views/survey.html',
+                    controller: 'prd.survey.controller'
+                } 
+            ).
+        otherwise (
+                {
+                  redirectTo: '/index.html'
+                }
+        );
+}]);
