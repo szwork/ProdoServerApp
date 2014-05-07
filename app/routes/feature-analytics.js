c76009ba4bcb0a008c2d63bf22c8ab82167652e1
DineshSawant <dinesh@giantleapsystems.com>
2014-05-07 18:54:05 +0530

    Done Api for Dashboard

--- app/routes/feature-analytics.js
+++ app/routes/feature-analytics.js
@@ -3,5 +3,6 @@ var auth=require('../common/js/security');
 
 exports.init = function (app) {
   app.get('/api/featureanalytics/:prodle',auth,api.featureanalytics.getTagAnalyticsPieChart);
-  app.get('/api/featureanalytics/barchart/:prodle',auth,api.featureanalytics.getTagAnalyticsForBarChart);
+  app.get('/api/featureanalytics/barchart/:prodle',auth,api.featureanalytics.getTagAnalyticsForBarChart);  
+  app.get('/api/trendingchart/:prodle',auth,api.featureanalytics.getDatewiseTrendingForProduct);
 }
