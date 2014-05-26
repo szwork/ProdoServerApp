c4058720e9e24b963ba5331e9049c437dce44a80
sunilmore690 <sunil@giantleapsystems.com>
2014-05-26 19:16:55 +0530

    all testimonial funcitonality done

--- app/testimonial/js/testimonial-api.js
+++ app/testimonial/js/testimonial-api.js
@@ -1,34 +1,80 @@
 var Testimonial=require("./testimonial");
+var logger = require("../../common/js/logger");
+
 exports.addTestimonial=function(req,res){
 	var orgid=req.params.orgid;
     
     var prodle=req.params.prodle;
   	var testimonialdata=req.body.testimonialdata;
     logger.emit("log","req testimonial body"+JSON.stringify(req.body));
-  	var product = new Product(testimonialdata);
+  	var testimonial = new Testimonial(testimonialdata);
 
   
   	var sessionuserid=req.user.userid;
      logger.emit("log","\norgid:"+orgid+"\nsessionid:"+sessionuserid);
-    product.removeAllListeners("failedAddTestimonial");
-    product.on("failedAddTestimonial",function(err){
+    testimonial.removeAllListeners("failedAddTestimonial");
+    testimonial.on("failedAddTestimonial",function(err){
       logger.emit("error", err.error.message,sessionuserid);
       // product.removeAllListeners();
       res.send(err);
     });
-    product.removeAllListeners("successfulAddTestimonial");
-    product.on("successfulAddTestimonial",function(result){
+    testimonial.removeAllListeners("successfulAddTestimonial");
+    testimonial.on("successfulAddTestimonial",function(result){
+      logger.emit("info", result.success.message,sessionuserid);
+      // product.removeAllListeners();
+      res.send(result);
+    });
+    if(req.user.org.orgid==orgid){ 
+      logger.emit("error","You can not write testimonial of your own product",sessionuserid)
+      testimonial.emit("failedAddTestimonial",{"error":{"code":"EA001","message":"You can not write testimonial of your own product"}})
+   	}else{
+     testimonial.addTestimonial(orgid,prodle,sessionuserid,req.get('host'));
+    }
+}
+exports.testimonialAction=function(req,res){
+	var testimonialid=req.params.testimonialid;
+	var action=req.query.name;
+    var testimonial = new Testimonial();
+    var sessionuserid=req.user.userid;
+     // logger.emit("log","\norgid:"+orgid+"\nsessionid:"+sessionuserid);
+    testimonial.removeAllListeners("failedTestimonailAction");
+    testimonial.on("failedTestimonailAction",function(err){
+      logger.emit("error", err.error.message,sessionuserid);
+      // product.removeAllListeners();
+      res.send(err);
+    });
+    testimonial.removeAllListeners("successfulTestimonialAction");
+    testimonial.on("successfulTestimonialAction",function(result){
+      logger.emit("info", result.success.message,sessionuserid);
+      // product.removeAllListeners();
+      res.send(result);
+    });
+    if(req.user.org.orgid==null){ 
+      logger.emit("error","You are not organization user To perform this action",sessionuserid)
+      testimonial.emit("failedAddTestimonial",{"error":{"code":"EA001","message":"You are not organization user To perform this action"}})
+   	}else{
+     testimonial.testimonialAction(testimonialid,sessionuserid,action);
+    }
+}
+
+exports.getTestimonialForProduct=function(req,res){
+	var prodle=req.params.prodle;
+    var testimonial = new Testimonial();
+    var sessionuserid=req.user.userid;
+     // logger.emit("log","\norgid:"+orgid+"\nsessionid:"+sessionuserid);
+    testimonial.removeAllListeners("failedTestimonialForProduct");
+    testimonial.on("failedTestimonialForProduct",function(err){
+      logger.emit("error", err.error.message,sessionuserid);
+      // product.removeAllListeners();
+      res.send(err);
+    });
+    testimonial.removeAllListeners("successfulTestimonialForProduct");
+    testimonial.on("successfulTestimonialForProduct",function(result){
       logger.emit("info", result.success.message,sessionuserid);
       // product.removeAllListeners();
       res.send(result);
     });
     
+     testimonial.getTestimonialForProduct(prodle);
    
-    
-    if(req.user.org.orgid==orgid){ 
-      logger.emit("error","You can not write testimonial of your own product",sessionuserid)
-      product.emit("failedAddTestimonial",{"error":{"code":"EA001","message":"You can not write testimonial of your own product"}})
-   	}else{
-     product.addTestimonial(orgid,prodle,sessionuserid);
-    }
 }
--- app/testimonial/js/testimonial.js
+++ app/testimonial/js/testimonial.js
@@ -1,21 +1,28 @@
 var events = require("events");
 var logger = require("../../common/js/logger");
 var ProductModel = require("../../product/js/product-model");
+var orgModel=require("../../org/js/org-model");
 var TestimonialModel=require("./testimonial-model");
+var CONFIG = require('config').Prodonus;
+var commonapi = require('../../common/js/common-api');
+var UserModel=require("../../user/js/user-model");
+var S=require("string");
+var __=require("underscore");
+var InboxModel=require("../../inbox/js/inbox-model");
 var Testimonial = function(testimonialdata) {
 	this.testimonial = testimonialdata;
 };
 
 Testimonial.prototype = new events.EventEmitter;
-module.exports = Product;
-Testimonial.prototype.addTestimonial=function(orgid,sessionuserid){
+module.exports = Testimonial;
+Testimonial.prototype.addTestimonial=function(orgid,prodle,sessionuserid,host){
 	var self=this;
 	var testimonialdata=this.testimonial;
 	////////////////////////////////////////////////////////////
-	_validateTestimonialData(self,testimonialdata,orgid,prodle,sessionuserid);
+	_validateTestimonialData(self,testimonialdata,orgid,prodle,sessionuserid,host);
 	//////////////////////////////////////////////////////////
 }
-var _validateTestimonialData=function(self,testimonialdata,orgid,prodle,sessionuserid){
+var _validateTestimonialData=function(self,testimonialdata,orgid,prodle,sessionuserid,host){
 	if(testimonialdata==undefined){
 		self.emit("failedAddTestimonial",{error:{code:"AV001",message:"please pass testimonialdata"}})
 	}else if(testimonialdata.text==undefined || testimonialdata.text==""){
@@ -24,19 +31,19 @@ var _validateTestimonialData=function(self,testimonialdata,orgid,prodle,sessionu
 		self.emit("failedAddTestimonial",{error:{code:"AV001",message:"please enter testimonial displayname"}})
 	}else{
 		//////////////////////////////////////
-		_addTestimonial(self,testimonialdata,orgid,prodle,sessionuserid)
+		_addTestimonial(self,testimonialdata,orgid,prodle,sessionuserid,host)
 		/////////////////////////////////////
 	}
 }
-var _addTestimonial=function(self,testimonialdata,orgid,prodle,sessionuserid){
-	ProductModel.findOne({orgid:orgid,prodle:prodle},{prodle:1},function(err,product){
+var _addTestimonial=function(self,testimonialdata,orgid,prodle,sessionuserid,host){
+	ProductModel.findOne({orgid:orgid,prodle:prodle},{prodle:1,name:1},function(err,product){
 		if(err){
 			logger.emit("error","Database Issue _addTestimonial"+err)
 			self.emit("failedAddTestimonial",{error:{code:"ED001",message:"Database Issue"}})
 		}else if(!product){
 			self.emit("failedAddTestimonial",{error:{message:"prodle or orgid is wrong"}})
 		}else{
-			UserModel.findOne({userid:sessionuserid},{profile_pic:1,userid:1,org:1},function(err,user){
+			UserModel.findOne({userid:sessionuserid},{username:1,email:1,profile_pic:1,userid:1,org:1},function(err,user){
 				if(err){
 					logger.emit("error","Database Issue _addTestimonial"+err)
 			    self.emit("failedAddTestimonial",{error:{code:"ED001",message:"Database Issue"}})
@@ -50,8 +57,11 @@ var _addTestimonial=function(self,testimonialdata,orgid,prodle,sessionuserid){
 							logger.emit("error","Database Issue _addTestimonial"+err)
 			        self.emit("failedAddTestimonial",{error:{code:"ED001",message:"Database Issue"}})
 						}else{
+							//////////////////////////////////////
+							_sendTestimonialRequestToOrganizationMember(self,testimonial,user,product,host);
+							//////////////////////////////////
 							///////////////////////////////////
-							_successfullAddTestimonial(self)
+							// _successfullAddTestimonial(self)
 							///////////////////////////////////
 						}
 					})
@@ -62,4 +72,214 @@ var _addTestimonial=function(self,testimonialdata,orgid,prodle,sessionuserid){
 }
 var _successfullAddTestimonial=function(self){
 	self.emit("successfulAddTestimonial",{success:{message:"Scuccessfully added testimonial"}});
+}
+var _sendTestimonialRequestToOrganizationMember=function(self,testimonial,user,product,host){
+	logger.emit("log","host"+host);
+	var body="";
+	var subject="Testimonial Requst";
+	body+="<br>Customer <username> <orgname> has provided a testimonial for our product <b><productname></b>.<br>";
+	body+="<br><br> '<b><testimonialtext></b>'<br><br>Please press or click <a  style='color:black;border:1;background-color:orange' href='http://"+host+"/api/testimonialaction/<testimonialid>?name=accept'>ACCEPT</a> if you want to make this testimonial public. And press or click <a style='color:black;border:1;background-color:orange' href='http://"+host+"/api/testimonialaction/<testimonialid>?name=reject' >REJECT</a> to remove the testimonial from product page"
+	body=S(body);
+
+	body=body.replaceAll("<username>",user.username);
+	if(user.org.orgname==undefined || user.org.orgname==null ){
+		body=body.replaceAll("<orgname>","");
+	}else{
+		body=body.replaceAll("<orgname>","from "+user.org.orgname);
+	}
+	
+	body=body.replaceAll("<productname>",product.name);
+	body=body.replaceAll("<testimonialid>",testimonial.testimonialid);
+	body=body.replaceAll("<testimonialtext>",testimonial.text);
+	console.log('body'+body)
+ body=body.s;
+	// body="<br><br>User <username> has added Testimonial h<b>"+product.name+"</b><br><br>";
+	body+="<br><br>This email content is sent on behalf of  "+user.email+" by Prodonus Software Team";
+  body+="<br>Disclaimer: We are not responsible for the content of this email as it is produced by "+user.email;
+    var grparray=[new RegExp("admin",'i'),new RegExp("marketing",'i'),new RegExp("marketing",'i')];
+		orgModel.aggregate({$match:{orgid:testimonial.orgid}},{$unwind:"$usergrp"},{$match:{"usergrp.grpname":{$in:grparray}}},{$project:{grpname:"$usergrp.grpname",grpmembers:"$usergrp.grpmembers",_id:0}},function(err,usergrps){
+			if(err){
+				logger.emit("error","Database Issue _sendProductEnquiryRequest "+err)
+		    self.emit("failedAddTestimonial",{"error":{"code":"ED001","message":"Database Issue"}})			
+			}else if(usergrps.length==0){
+				self.emit("failedAddTestimonial",{"error":{"message":"There is no admin,sales,marketing user exists"}})			
+			}else{
+				var usergrpidsarray=[];
+				for(var j=0;j<usergrps.length;j++){
+					usergrpidsarray=__.union(usergrpidsarray,usergrps[j].grpmembers);
+				}
+				UserModel.find({userid:{$in:usergrpidsarray},status:"active"},{userid:1,email:1},function(err,useremails){
+  				if(err){
+  					logger.emit("error","Database Issue _sendProductEnquiryRequest "+err)
+						self.emit("failedAddTestimonial",{"error":{"message":"Database Issue"}})			
+  				}else if(useremails.length==0){
+  					self.emit("failedAddTestimonial",{"error":{"message":"No Organization member exists"}})			
+  				}else{
+  					var emailarray=[];
+  					var validproductenquiryuserids=[];
+						for(var i=0;i<useremails.length;i++){
+							emailarray.push(useremails[i].email);	
+							validproductenquiryuserids.push(useremails[i].userid);	
+						}
+						var message = {
+		        from: "Prodonus  <business@prodonus.com>", // sender address
+		        to: emailarray+"", // list of receivers
+		        subject:subject, // Subject line
+		        html: body // html body
+		       };
+		      
+         ////////////////////////////////////
+		   _addProductTestimonialToGroupMemberInbox(message,validproductenquiryuserids,user)
+		     ///////////////////////////////////
+         	///////////////////////////////////
+							 _successfullAddTestimonial(self)
+							///////////////////////////////////
+
+		    commonapi.sendMail(message,CONFIG.smtp_business, function (result){
+		      if(result=="failure"){
+		        logger.emit("error","Product enquiry request not sent to "+message.to+" by"+user.email);
+		      }else{
+		        logger.emit("log","Product enquiry request Sent Successfully to"+message.to+" by"+user.email);
+		      }
+		    });
+		  }
+		})
+	}
+ })
+}
+var _addProductTestimonialToGroupMemberInbox=function(message,userids,user){
+	UserModel.find({userid:{$in:userids}},{userid:1,email:1,firstname:1},function(err,users){
+		if(err){
+			logger.emit("error","Database Issue"+err)
+		}else if(!users){
+			logger.emit("error","No user exists _addProductTestimonialToGroupMemberInbox")
+		}else{
+			var inboxarray=[]
+			for(var i=0;i<users.length;i++){
+				var inbox
+				if(user.firstname==undefined){
+					inbox={messagetype:"testimonial",userid:users[i].userid,from:{email:user.email,userid:user.userid},subject:message.subject,body:message.html}
+				}else{
+					inbox={messagetype:"testimonial",userid:users[i].userid,from:{email:user.email,userid:user.userid,name:user.firstname},subject:message.subject,body:message.html}
+				}
+				inboxarray.push(inbox)
+			}
+			InboxModel.create(inboxarray,function(err,inboxex){
+				if(err){
+					logger.emit("error","Database Issue"+err)
+				}else{
+					logger.emit("log","sent to inbox")
+				}
+			})
+
+		}
+	})
+}
+Testimonial.prototype.testimonialAction=function(testimonialid,sessionuserid,action){
+	var self=this;
+	////////////////////////////////////////////////////////////////
+	_isAuthorizedToPerformAction(self,testimonialid,sessionuserid,action)
+	///////////////////////////////////////////////////////////////
+}
+var _isAuthorizedToPerformAction=function(self,testimonialid,sessionuserid,action){
+	TestimonialModel.findOne({testimonialid:testimonialid},function(err,testimonial){
+		if(err){
+			logger.emit("error","Database Issue _isAuthorizedToPerformAction "+err)
+		  self.emit("failedTestimonailAction",{"error":{"code":"ED001","message":"Database Issue"}})			
+		}else if(!testimonial){
+			self.emit("failedTestimonailAction",{"error":{"message":"testimonialid does not exists"}})			
+		}else{
+			UserModel.findOne({userid:sessionuserid},{org:1,userid:1},function(err,user){
+				if(err){
+					logger.emit("error","Database Issue _isAuthorizedToPerformAction "+err)
+		      self.emit("failedTestimonailAction",{"error":{"code":"ED001","message":"Database Issue"}})			
+				}else if(!user){
+					self.emit("failedTestimonailAction",{"error":{"message":"userid is wrong"}})			
+				}else{
+					if(user.org.orgid!=testimonial.orgid){
+						self.emit("failedTestimonailAction",{"error":{"message":"Its not your product to accept or reject testimonial"}})			
+					}else{
+						///////////////////////////////
+						_performTestimonialAction(self,testimonial,action)
+						///////////////////////////////
+					}
+				}
+			})	
+		}
+	})
+}
+var _performTestimonialAction=function(self,testimonial,action){
+	if(action=="accept"){
+		if(testimonial.status==action){
+			self.emit("failedTestimonailAction",{error:{message:"Testimonial already accepted"}})	
+		}else{
+			///////////////////////////////////////////
+			_acceptTestimonial(self,testimonial,action)
+			///////////////////////////////////////////
+		}
+	}else{
+		if(testimonial.status==action){
+			self.emit("failedTestimonailAction",{error:{message:"Testimonial already rejected"}})	
+		}else{
+			///////////////////////////////////////////
+			_rejectTestimonial(self,testimonial,action)
+			///////////////////////////////////////////
+		}
+	}
+}
+var _acceptTestimonial=function(self,testimonial,action){
+	TestimonialModel.update({testimonialid:testimonial.testimonialid},{$set:{status:"accept"}},function(err,testimonialstatus){
+		if(err){
+			logger.emit("error","Database Issue _acceptTestimonial "+err)
+		  self.emit("failedTestimonailAction",{"error":{"code":"ED001","message":"Database Issue"}})			
+		}else if(testimonialstatus==0){
+			self.emit("failedTestimonailAction",{"error":{"message":"testimonialid is wrong"}})			
+		}else{
+			var message="Successfully accepted the testimonial";
+			////////////////////////////////
+			_successfullTestimonialAction(self,message)
+			////////////////////////////////	
+		}
+	})
+}
+var _rejectTestimonial=function(self,testimonial,action){
+	TestimonialModel.remove({testimonialid:testimonial.testimonialid},function(err,testimonialstatus){
+		if(err){
+			logger.emit("error","Database Issue _rejectTestimonial "+err)
+		  self.emit("failedTestimonailAction",{"error":{"code":"ED001","message":"Database Issue"}})			
+		}else if(testimonialstatus==0){
+			self.emit("failedTestimonailAction",{"error":{"message":"testimonialid is wrong"}})			
+		}else{
+			var message="Successfully rejected the testimonial";
+			////////////////////////////////
+			_successfullTestimonialAction(self,message)
+			////////////////////////////////	
+		}
+	})
+}
+var _successfullTestimonialAction=function(self,message){
+	self.emit("successfulTestimonialAction",{success:{message:message}})
+}
+Testimonial.prototype.getTestimonialForProduct=function(prodle){
+	var self=this;
+	//////////////////////////////////////
+	_getTestimonialForProduct(self,prodle)
+	/////////////////////////////////
+}
+var _getTestimonialForProduct=function(self,prodle){
+	TestimonialModel.find({prodle:prodle,status:"accept"},function(err,testimonials){
+		if(err){
+			logger.emit("error","Database Issue _getTestimonialForProduct "+err)
+		  self.emit("failedTestimonialForProduct",{"error":{"code":"ED001","message":"Database Issue"}})			
+		}else if(testimonials.length==0){
+			self.emit("failedTestimonialForProduct",{error:{message:"No testimonials for product"}})
+		}else{
+			//////////////////////////////////
+			_successfullGetTestimonialForProduct(self,testimonials)
+			////////////////////////////////
+		}
+	})
+}
+var _successfullGetTestimonialForProduct=function(self,testimonials){
+	self.emit('successfulTestimonialForProduct',{success:{message:"Getting testimonials for product successfully",testimonials:testimonials}})
 }
