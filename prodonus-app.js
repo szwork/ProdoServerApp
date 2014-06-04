96113ccc900b3c4eb3a9ddca5b5c2c5c544f1cc7
DineshSawant <dinesh@giantleapsystems.com>
2014-06-04 13:31:45 +0530

    Done Changes Headers

--- prodonus-app.js
+++ prodonus-app.js
@@ -61,6 +61,15 @@ app.use(function(req, res, next) {
   req.headers['if-none-match'] = 'no-match-for-this';
   next();  
 });
+app.all('*',  function(req,  res,  next)  {
+  res.header("Access-Control-Allow-Origin",  "*");
+  res.header('Access-Control-Allow-Methods',  'OPTIONS,GET,POST,PUT,DELETE');
+  res.header("Access-Control-Allow-Headers",  "Origin, X-Requested-With, Content-Type, Accept");
+    if('OPTIONS'  ==  req.method){
+      return  res.send(200);
+    }
+    next();
+});
 
 /////to disable no cache always give 200 statuscode for eache request
 app.disable('etag');
